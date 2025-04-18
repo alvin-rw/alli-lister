5c98c4b671d27a6a7a314c7131575c405ec367ad
Alvin Wonys <alvin.rw@proton.me>
2025-04-18 23:57:30 +0800

    feat: add all-regions parameter to allow user to list all lambda functions information from all opted-in region in their aws account.

--- cmd.go
+++ cmd.go
@@ -32,17 +32,19 @@ const (
 
 // getAllLambdaFunctionsDetails returns slice containing the details of all
 // Lambda functions in the region specified by regions parameter
-func (app *application) getAllLambdaFunctionsDetails(regions []string) ([]lambdaFunction, error) {
-	app.logger.Info("getting function details for all lambda functions",
-		zap.Strings("regions", regions),
-	)
+func (app *application) getAllLambdaFunctionsDetails() ([]lambdaFunction, error) {
+	app.logger.Info("getting function details for lambda functions")
 
 	var lambdaFunctionsList []lambdaFunction
-	for _, region := range regions {
-		in := &lambda.ListFunctionsInput{}
+	in := &lambda.ListFunctionsInput{}
+
+	for _, lambdaClient := range app.lambdaClients {
+		app.logger.Debugw("getting Lambda functions",
+			zap.String("current_region", lambdaClient.Options().Region),
+		)
 
 		for {
-			out, err := app.lambdaClient.ListFunctions(context.Background(), in)
+			out, err := lambdaClient.ListFunctions(context.Background(), in)
 			if err != nil {
 				return nil, err
 			}
@@ -50,6 +52,7 @@ func (app *application) getAllLambdaFunctionsDetails(regions []string) ([]lambda
 			for _, functionDetail := range out.Functions {
 				f := lambdaFunction{
 					Name:         *functionDetail.FunctionName,
+					Region:       lambdaClient.Options().Region,
 					Arn:          *functionDetail.FunctionArn,
 					Description:  *functionDetail.Description,
 					LastModified: *functionDetail.LastModified,
@@ -104,8 +107,6 @@ func (app *application) getAllLambdaFunctionsLastInvokeTime(lambdaFunctionsList 
 func (app *application) getLambdaFunctionLastInvokeTime(jobs <-chan job, lambdaFunctionsList []lambdaFunction, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	time.Sleep(10 * time.Second)
-
 	for currentJob := range jobs {
 		logGroupName := fmt.Sprintf("%s%s", lambdaLogGroupPrefix, currentJob.functionName)
 
@@ -116,41 +117,45 @@ func (app *application) getLambdaFunctionLastInvokeTime(jobs <-chan job, lambdaF
 			OrderBy:      types.OrderByLastEventTime,
 		}
 
-		out, err := app.cwlogsClient.DescribeLogStreams(context.Background(), input)
-		if err != nil {
-			var oe *smithy.OperationError
-			if errors.As(err, &oe) {
-				if oe.Operation() == "DescribeLogStreams" && strings.Contains(oe.Unwrap().Error(), cloudWatchLogGroupDoesNotExistErrorMessage) {
-					app.logger.Debugw("CloudWatch log group does not exist for lambda function",
-						zap.String("function_name", currentJob.functionName),
+		// TODO: check concurrency logic and make sure that the describe is working as intended
+		// TODO: make sure that the region used is the same for describing lambda function and describing cloudwatch logs
+		for _, cwLogsClient := range app.cwlogsClients {
+			out, err := cwLogsClient.DescribeLogStreams(context.Background(), input)
+			if err != nil {
+				var oe *smithy.OperationError
+				if errors.As(err, &oe) {
+					if oe.Operation() == "DescribeLogStreams" && strings.Contains(oe.Unwrap().Error(), cloudWatchLogGroupDoesNotExistErrorMessage) {
+						app.logger.Debugw("CloudWatch log group does not exist for lambda function",
+							zap.String("function_name", currentJob.functionName),
+						)
+
+						lambdaFunctionsList[currentJob.index].LastInvoked = "-"
+					}
+				} else {
+					app.logger.Debugw("error when describing log stream",
+						zap.String("log group name", logGroupName),
+						zap.Error(err),
 					)
-
-					lambdaFunctionsList[currentJob.index].LastInvoked = "-"
 				}
+			} else if len(out.LogStreams) == 0 {
+				app.logger.Debugw("no log stream exists for lambda function",
+					zap.String("function_name", currentJob.functionName),
+				)
+
+				lambdaFunctionsList[currentJob.index].LastInvoked = "-"
 			} else {
-				app.logger.Debugw("error when describing log stream",
-					zap.String("log group name", logGroupName),
-					zap.Error(err),
-				)
-			}
-		} else if len(out.LogStreams) == 0 {
-			app.logger.Debugw("no log stream exists for lambda function",
-				zap.String("function_name", currentJob.functionName),
-			)
+				if out != nil && out.LogStreams != nil && out.LogStreams[0].LastEventTimestamp != nil {
+					lastEventTimestampInSeconds := *out.LogStreams[0].LastEventTimestamp / 1000
+					t := time.Unix(lastEventTimestampInSeconds, 0)
 
-			lambdaFunctionsList[currentJob.index].LastInvoked = "-"
-		} else {
-			if out != nil && out.LogStreams != nil && out.LogStreams[0].LastEventTimestamp != nil {
-				lastEventTimestampInSeconds := *out.LogStreams[0].LastEventTimestamp / 1000
-				t := time.Unix(lastEventTimestampInSeconds, 0)
-
-				lambdaFunctionsList[currentJob.index].LastInvoked = t.Format("2006-01-02T15:04:05-07:00")
-				app.logger.Debugw("last invoke time info",
-					zap.Int64("*out.LogStreams[0].LastEventTimestamp", *out.LogStreams[0].LastEventTimestamp/1000),
-					zap.Int64("lastEventTimestampInSeconds", lastEventTimestampInSeconds),
-					zap.String("formatted time", t.Format("2006-01-02T15:04:05-07:00")),
-					zap.String("lambdaFunctionsList[index].lastInvoked", lambdaFunctionsList[currentJob.index].LastInvoked),
-				)
+					lambdaFunctionsList[currentJob.index].LastInvoked = t.Format("2006-01-02T15:04:05-07:00")
+					app.logger.Debugw("last invoke time info",
+						zap.Int64("*out.LogStreams[0].LastEventTimestamp", *out.LogStreams[0].LastEventTimestamp/1000),
+						zap.Int64("lastEventTimestampInSeconds", lastEventTimestampInSeconds),
+						zap.String("formatted time", t.Format("2006-01-02T15:04:05-07:00")),
+						zap.String("lambdaFunctionsList[index].lastInvoked", lambdaFunctionsList[currentJob.index].LastInvoked),
+					)
+				}
 			}
 		}
 	}
--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"os"
+	"sync"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
@@ -24,18 +29,18 @@ type settings struct {
 
 // application stores main program global dependencies
 type application struct {
-	logger       *zap.SugaredLogger
-	ec2Client    *ec2.Client
-	lambdaClient *lambda.Client
-	cwlogsClient *cloudwatchlogs.Client
+	logger        *zap.SugaredLogger
+	ec2Client     *ec2.Client
+	lambdaClients []*lambda.Client
+	cwlogsClients []*cloudwatchlogs.Client
 }
 
 func main() {
 	var stg settings
 	flag.BoolVar(&stg.debug, "debug", false, "Debug mode. Shows debug logs")
 	flag.StringVar(&stg.awsProfileName, "aws-profile", "default", "AWS Profile Name")
-	flag.BoolVar(&stg.getAllRegions, "all-regions", true, "Whether to get data from all AWS Regions")
-	flag.StringVar(&stg.outputFileName, "out-name", "lambda-list.csv", "The name of the output file")
+	flag.BoolVar(&stg.getAllRegions, "all-regions", false, "Whether to get data from all AWS Regions")
+	flag.StringVar(&stg.outputFileName, "output-file-name", "", "The name of the output file. If not provided, the resulting file name will be [timestamp].csv")
 	flag.IntVar(&stg.maxWorkers, "max-workers", 50, "Maximum number of workers")
 	flag.Parse()
 
@@ -51,88 +56,75 @@ func main() {
 		)
 	}
 
-	app := &application{
-		logger:    logger,
-		ec2Client: ec2.NewFromConfig(cfg),
+	app, err := initializeApplication(logger, cfg, stg.getAllRegions)
+	if err != nil {
+		logger.Fatalw("error when initializing application struct",
+			zap.Error(err),
+		)
 	}
 
-	regions := []string{}
-	if stg.getAllRegions {
-		regions, err = app.getAllOptedInRegions()
-		if err != nil {
-			app.logger.Fatalf("error when listing all available regions",
-				zap.Error(err),
-			)
-		}
-	} else {
-		regions = append(regions, cfg.Region)
-	}
-
-	lambdaClients := []*lambda.Client{}
-	cloudWatchClients := []*cloudwatchlogs.Client{}
-	for _, region := range regions {
-		lambdaClient := lambda.NewFromConfig(cfg, config.WithRegion(region))
-	}
-
-	lambdaFunctionsList, err := app.getAllLambdaFunctionsDetails(regions)
+	lambdaFunctionsList, err := app.getAllLambdaFunctionsDetails()
 	if err != nil {
 		logger.Fatalw("error when listing lambda function details",
 			zap.Error(err),
 		)
 	}
 
-	fmt.Println(lambdaFunctionsList)
+	wg := &sync.WaitGroup{}
+	app.getAllLambdaFunctionsLastInvokeTime(lambdaFunctionsList, wg, stg.maxWorkers)
+	wg.Wait()
 
-	// wg := &sync.WaitGroup{}
-	// app.getAllLambdaFunctionsLastInvokeTime(lambdaFunctionsList, wg, stg.maxWorkers)
-	// wg.Wait()
+	fileName := getFileName(stg.outputFileName)
 
-	// logger.Infof("writing the output to %q", stg.outputFileName)
-	// f, err := os.Create(stg.outputFileName)
-	// if err != nil {
-	// 	logger.Errorw("error when creating a file",
-	// 		zap.Error(err),
-	// 	)
-	// }
-	// defer f.Close()
+	logger.Infof("writing the output to %q", fileName)
+	f, err := os.Create(fileName)
+	if err != nil {
+		logger.Errorw("error when creating a file",
+			zap.Error(err),
+		)
+	}
+	defer f.Close()
 
-	// w := csv.NewWriter(f)
-	// defer w.Flush()
+	w := csv.NewWriter(f)
+	defer w.Flush()
 
-	// titles := lambdaFunctionsList[0].getTitleFields()
-	// err = w.Write(titles)
-	// if err != nil {
-	// 	logger.Errorw("error when writing title",
-	// 		zap.Error(err),
-	// 	)
-	// }
+	titles := lambdaFunctionsList[0].getTitleFields()
+	err = w.Write(titles)
+	if err != nil {
+		logger.Errorw("error when writing title",
+			zap.Error(err),
+		)
+	}
 
-	// for _, lambdaDetails := range lambdaFunctionsList {
-	// 	record := []string{
-	// 		lambdaDetails.Name,
-	// 		lambdaDetails.Arn,
-	// 		lambdaDetails.Description,
-	// 		lambdaDetails.LastModified,
-	// 		lambdaDetails.IamRole,
-	// 		lambdaDetails.Runtime,
-	// 		lambdaDetails.LastInvoked,
-	// 	}
+	for _, lambdaDetails := range lambdaFunctionsList {
+		record := []string{
+			lambdaDetails.Name,
+			lambdaDetails.Region,
+			lambdaDetails.Arn,
+			lambdaDetails.Description,
+			lambdaDetails.LastModified,
+			lambdaDetails.IamRole,
+			lambdaDetails.Runtime,
+			lambdaDetails.LastInvoked,
+		}
 
-	// 	err := w.Write(record)
-	// 	if err != nil {
-	// 		logger.Errorw("error when writing the entry",
-	// 			zap.String("function_name", lambdaDetails.Name),
-	// 			zap.Error(err),
-	// 		)
-	// 	}
-	// }
+		err := w.Write(record)
+		if err != nil {
+			logger.Errorw("error when writing the entry",
+				zap.String("function_name", lambdaDetails.Name),
+				zap.Error(err),
+			)
+		}
+	}
 
-	// logger.Infow("all the function details have been written to the output",
-	// 	zap.String("file name", stg.outputFileName),
-	// 	zap.Int("number of functions", len(lambdaFunctionsList)),
-	// )
+	logger.Infow("all the function details have been written to the output",
+		zap.String("file name", fileName),
+		zap.Int("number of functions", len(lambdaFunctionsList)),
+	)
 }
 
+// createLogger creates zap.SugaredLogger with debug or info logging level
+// depending on the input
 func createLogger(debugMode bool) *zap.SugaredLogger {
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
@@ -158,4 +150,71 @@ func createLogger(debugMode bool) *zap.SugaredLogger {
 	}
 
 	return zap.Must(config.Build()).Sugar()
+}
+
+// initializeApplication creates application struct with logger and AWS Service Clients (ec2Client, lambdaClients, and cwLogsClients).
+//
+// lambdaClients and cwLogsClients are created based on the number of regions.
+// If getAllRegions is set to true, it will populate the application struct with clients for all AWS Regions
+func initializeApplication(logger *zap.SugaredLogger, cfg aws.Config, getAllRegions bool) (*application, error) {
+	logger.Debug("initializing application struct")
+
+	app := &application{
+		logger:    logger,
+		ec2Client: ec2.NewFromConfig(cfg),
+	}
+
+	logger.Debugw("getting chosen regions",
+		zap.Bool("all_regions", getAllRegions),
+	)
+	// get regions list based on the chosen parameters
+	regions := []string{}
+	if getAllRegions {
+		allOptedInRegions, err := app.getAllOptedInRegions()
+		if err != nil {
+			app.logger.Fatalf("error when listing all available regions",
+				zap.Error(err),
+			)
+		}
+
+		regions = allOptedInRegions
+	} else {
+		// if no specified region is chosen, use AWS CLI default region
+		regions = append(regions, cfg.Region)
+	}
+
+	// lambdaClients and cwLogsClients will hold all the service clients from all chosen regions.
+	// This will be used to query the AWS Service
+	lambdaClients := []*lambda.Client{}
+	cwLogsClients := []*cloudwatchlogs.Client{}
+
+	logger.Debug("initializing service clients for chosen regions")
+	// Create AWS service clients for all chosen region and put it in the application struct
+	for _, region := range regions {
+		lambdaClient := lambda.NewFromConfig(cfg, func(o *lambda.Options) {
+			o.Region = region
+		})
+		lambdaClients = append(lambdaClients, lambdaClient)
+
+		cwLogsClient := cloudwatchlogs.NewFromConfig(cfg, func(o *cloudwatchlogs.Options) {
+			o.Region = region
+		})
+		cwLogsClients = append(cwLogsClients, cwLogsClient)
+	}
+	logger.Debug("service clients retrieved")
+
+	app.lambdaClients = lambdaClients
+	app.cwlogsClients = cwLogsClients
+
+	return app, nil
+}
+
+// getFileName generates file name based on the user input. If the user does not input a file name,
+// it returns filename with format [timestamp].csv, e.g. 1744990200.csv
+func getFileName(inputFileName string) string {
+	if inputFileName == "" {
+		return fmt.Sprintf("%d.csv", time.Now().Unix())
+	} else {
+		return inputFileName
+	}
 }
